95960920451e1afc2b47e20c6c91d36fd67a9a33
Ali <mythologyali@gmail.com>
2018-02-08 03:02:18 -0800

    commenting

--- alexa.py
+++ alexa.py
@@ -12,21 +12,29 @@ class Alexa:
 		print "CS: {}".format(self.cs)
 
 	def _get_cs(self):
+		"""
+		Getting the initial conversation start, this either has to be
+		saved to a database or fetched at startup every single time
+		"""
 		r = requests.get(self.cs_endpoint)
 		return r.json()['cs']
 
 	def _format_query_endpoint(self, text):
+		"""Fetching formatted endpoint """
 		return 'http://www.cleverbot.com/getreply?key={}&input={}&cs={}'\
 			.format(secret.CLEVERBOT_API_KEY, text, self.cs)
 
 	def _format_question(self, text):
+		"""Replacing the word alexa to avoid confusing cleverbot"""
 		return re.sub('alexa', 'cleverbot', text, re.IGNORECASE)
 
 	def say(self, text):
+		"""Ask alexa a question"""
 		print "Asking alexa..."
 		interaction = urllib.quote_plus(self._format_question(text))
 		query = self._format_query_endpoint(interaction)
 		r = requests.get(query)
-		print "Alexa replied: {}".format(r.json()['output'])
-		return r.json()['output']
+		reply = r.json()['output']
+		print "Alexa replied: {}".format(reply)
+		return reply
 

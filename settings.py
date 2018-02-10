2d7f5a40a3de9c9b311d0d007d13a5d9c95645a8
Ali <mythologyali@gmail.com>
2018-02-10 02:00:13 -0800

    settings

---
+++ settings.py
@@ -0,0 +1,4 @@
+CLEVERBOT_API_KEY   = ''  # cleverbot api key
+YOUTUBE_API_KEY     = ''  # youtube api key
+BOT_PHONE_NUMBER    = ''  # your bots phone number so it can recognize pings
+PROFILE             = ''  # path to your firefox profile
